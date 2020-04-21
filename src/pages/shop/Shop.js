758a10d39dfaf912a25a26d11baaf984f88f60f2
Luciano <luchosr@gmail.com>
2020-04-21 19:24:57 -0300

    add Collection Items styles

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import SHOP_DATA from "./shop.data";
-import PreviewCollection from "../../components/previewCollection/PreviewCollection";
+import PreviewCollection from "../../components/collectionPreview/CollectionPreview";
+import CollectionPreview from "../../components/collectionPreview/CollectionPreview";
 
 const Shop = (props) => {
   const [collections, setCollections] = useState(SHOP_DATA);
   return (
     <div className="shop-page">
       {collections.map(({ id, ...otherCollectionProps }) => (
-        <PreviewCollection key={id} {...otherCollectionProps} />
+        <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
     </div>
   );
