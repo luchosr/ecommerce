9f52760324cef5cd07bd8925200b954d043322b0
Luciano <luchosr@gmail.com>
2020-06-05 18:13:57 -0300

    :sparkles: add cart item component

---
+++ src/components/cartItem/CartItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./CartItem";
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt="item" />
+      <div className="item-deatils">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
