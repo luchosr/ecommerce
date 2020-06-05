0dc61ffc163a467a2f49270365833ed50a9a5fed
Luciano <luchosr@gmail.com>
2020-06-04 21:23:37 -0300

    add cart toggle feature

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,8 +5,10 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./HeaderStyles.scss";
 
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cartIcon/CartIcon";
+import CartDropdown from "../cartDropdown/CartDropdown";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -28,13 +30,16 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
