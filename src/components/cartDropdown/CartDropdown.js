91d4d8ee9f040989054c2039daa96b8cd9baab31
Luciano <luchosr@gmail.com>
2020-06-08 18:04:31 -0300

    :beetle: fix cart icon bug

--- src/components/cartDropdown/CartDropdown.js
+++ src/components/cartDropdown/CartDropdown.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../customButton/CustomButton";
 import CartItem from "../cartItem/CartItem";
+import { selectCartItems } from "../../redux/cart/cartSelectors";
 
 import "./CartDropdown.scss";
 
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
