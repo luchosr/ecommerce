f645242f9045b8a3669cb7c95c7038bb7a023718
Luciano <luchosr@gmail.com>
2020-06-11 19:08:19 -0300

    :sparkles: add Checkout Item component

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import CheckoutItem from "../../components/checkoutItem/CheckoutItem";
 
 import { selectCartItems } from "../../redux/cart/cartSelectors";
 
@@ -26,7 +27,9 @@ const Checkout = ({ cartItems, total }) => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map((cartItem) => cartItem.name)}
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
