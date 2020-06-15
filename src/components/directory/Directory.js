8cb0f0649a7a2dda5dc7a272e645a3679892ebc6
Luciano <luchosr@gmail.com>
2020-06-15 19:56:49 -0300

    :sparkles: update directory selector

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -1,43 +1,14 @@
 import React, { useState } from "react";
 import "./Directory.scss";
 import MenuItem from "../menuItem/MenuItem";
+import { createStructuredSelector } from "reselect";
 
-const Directory = () => {
-  const [sections, setsSections] = useState([
-    {
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      id: 1,
-      linkUrl: "hats",
-    },
-    {
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      id: 2,
-      linkUrl: "",
-    },
-    {
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      id: 3,
-      linkUrl: "",
-    },
-    {
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
-      id: 4,
-      linkUrl: "",
-    },
-    {
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
-      id: 5,
-      linkUrl: "",
-    },
-  ]);
+import { selectDirectorySections } from "../../redux/directory/directorySelector";
 
+import "./Directory.scss";
+import { connect } from "react-redux";
+
+const Directory = ({ sections }) => {
   return (
     <div className="directory-menu">
       {sections.map(({ id, ...otherSectionProps }) => (
@@ -47,4 +18,8 @@ const Directory = () => {
   );
 };
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps)(Directory);
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import { connect } from "react-redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directoryReducer";
 
 const persistConfig = {
   key: "root",
@@ -14,6 +16,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
