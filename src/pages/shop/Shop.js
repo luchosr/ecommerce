4dedacb49df64a96170659a0ef8ec36ffb19131c
Luciano <luchosr@gmail.com>
2020-06-17 11:06:35 -0300

    :sparkles: add collections overview component

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import PreviewCollection from "../../components/collectionPreview/CollectionPreview";
-import CollectionPreview from "../../components/collectionPreview/CollectionPreview";
 
-import { selectCollections } from "../../redux/shop/shopSelector";
+import CollectionsOverview from "../../components/collectionsOverview/CollectionsOverview";
+
 const Shop = ({ collections }) => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
 
 export default Shop;
