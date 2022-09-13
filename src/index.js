e741f484141c2fa3372607cced4046c8b9d9ad9e
Luciano Ramello <luchosr@gmail.com>
2022-09-13 17:59:19 +0200

    update navigation and index styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import "./index.css";
-import App from "./App";
+import './index.scss';
+import App from './App';
 
-import { store, persistor } from "./redux/store";
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,5 +20,5 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
 
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,23 @@
 import {Fragment} from 'react';
-import  {Outlet} from 'react-router-dom'
+import  {Outlet, Link} from 'react-router-dom'
+
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
         <>
             <div className="navigation">
-                <h1> Iam the navigation bar</h1>
+                <Link className="logo-container" to='/'>
+               <CrwnLogo className='logo'/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="shop">
+                        SHOP
+                    </Link>
+                </div>
             </div>
             <Outlet/>
         </>
