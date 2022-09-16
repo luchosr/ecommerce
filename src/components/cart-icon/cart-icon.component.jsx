4f7b372e076363fb308f4973037d6db44fe0736c
Luciano Ramello <luchosr@gmail.com>
2022-09-16 12:49:34 +0200

    udpate cart context

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,27 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import { toggleCartHidden } from '../../redux/cart/cartActions';
-import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
+import { useContext } from 'react';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
+import { CartContext } from '../../context/cart.context';
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon-container' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>{itemCount}</span>
-  </div>
-);
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
 
-// const mapDispatchToProps = (dispatch) => ({
-//   toggleCartHidden: () => dispatch(toggleCartHidden()),
-// });
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
-// const mapStateToProps = createStructuredSelector({
-//   itemCount: selectCartItemsCount,
-// });
+  return (
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>0</span>
+    </div>
+  );
+};
 
-// export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
 export default CartIcon;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,13 +6,15 @@ import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../context/user.context';
-
+import { CartContext } from '../../context/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+
   return (
     <>
       <div className='navigation'>
@@ -34,7 +36,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
