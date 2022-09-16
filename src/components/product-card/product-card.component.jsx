da57ca1f737488c5fccc33caa121b7635e96106f
Luciano Ramello <luchosr@gmail.com>
2022-09-16 10:28:52 +0200

    add product card

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,1 +1,15 @@
 import './product-card.styles.scss';
+import Button from '../button/button.component';
+
+const ProductCard = () => {
+  <div className='product-card-container'>
+    <img src='' alt='' />
+    <div className='footer'>
+      <span className='name'></span>
+      <span className='price'></span>
+    </div>
+    <Button buttonType='inverted'></Button>
+  </div>;
+};
+
+export default ProductCard;
