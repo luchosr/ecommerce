7f5307e0a2d0dba8f4744314e0c155dab60e4323
Luciano Ramello <luchosr@gmail.com>
2022-09-19 20:08:25 +0200

    update cart item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import './cart-item.styles.scss';
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity } = cartItem;
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <div className='cart-item'>
-      <h2 className='name'>{name}</h2>
+    <div className='cart-item-container'>
+      <img src={imageUrl} alt={name} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
+          {quantity} x ${price}
+        </span>
+      </div>
       <span>{quantity}</span>
     </div>
   );
