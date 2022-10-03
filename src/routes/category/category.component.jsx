588b73d3c1b116120c10b47dad0ef36c18c53960
Luciano <luciano.ramello@ext.soprabanking.com>
2022-10-03 17:32:29 +0200

    update category styles

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { CategoriesContext } from "../../context/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import "./category.styles.scss";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className="category-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default Category;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
-// import Category from "../category/category.component";
+import Category from "../category/category.component";
 
 import "./shop.styles.scss";
 
@@ -9,7 +9,7 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      {/* <Route path=":category" element={<Category />} /> */}
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
