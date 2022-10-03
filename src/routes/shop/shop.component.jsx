673dca3502edfd344d7a1508ecfdda2a1b895f69
Luciano <luciano.ramello@ext.soprabanking.com>
2022-10-03 15:55:24 +0200

    update shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
@@ -10,13 +10,18 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
-      <div className="products-container">
-        {/* {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))} */}
-      </div>
-    </>
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
 
