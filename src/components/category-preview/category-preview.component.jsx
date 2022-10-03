d8e8641e67de743256519549bfd388223f1c038f
Luciano <luciano.ramello@ext.soprabanking.com>
2022-10-03 16:02:14 +0200

    add category preview component and styles

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./category-preview.styles.sass";
+
+const CategoryPreview = () => {
+  return <div className="category-preview-container"></div>;
+};
+
+export default CategoryPreview;
