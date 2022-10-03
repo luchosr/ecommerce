454ec481204a39e16b99cb52d68609dcb59dbc22
Luciano <luciano.ramello@ext.soprabanking.com>
2022-10-03 15:35:32 +0200

    add Categories Context

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import './index.scss';
-import App from './App';
-import { UserProvider } from './context/user.context';
-import { ProductsProvider } from './context/products.context';
-import { CartProvider } from './context/cart.context';
+import "./index.scss";
+import App from "./App";
+import { UserProvider } from "./context/user.context";
+import { CategoriesProvider } from "./context/categories.context";
+import { CartProvider } from "./context/cart.context";
 
-import { store, persistor } from './redux/store';
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <PersistGate persistor={persistor}>
               <App />
             </PersistGate>
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-    {/* </Provider> */}
   </React.StrictMode>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { ProductsContext } from '../../context/products.context';
+import { CategoriesContext } from "../../context/categories.context";
 
-import './shop.styles.scss';
+import "./shop.styles.scss";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { products } = useContext(CategoriesContext);
 
   return (
-    <div className='products-container'>
+    <div className="products-container">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
