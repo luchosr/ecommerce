49b72abe237134ccf99ae401573ec832af27d38c
Luciano <luciano.ramello@ext.soprabanking.com>
2022-10-03 16:15:42 +0200

    update shop component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import ProductCard from "../product-card/product-card.component";
+import "./category-preview.styles.scss";
 
-import "./category-preview.styles.sass";
-
-const CategoryPreview = () => {
-  return <div className="category-preview-container"></div>;
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span className="title">{title.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPreview;
