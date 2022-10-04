67ba45c524b38bc62b0ce554fe6ad53d16ed2c40
Luciano Ramello <luchosr@gmail.com>
2022-10-04 16:29:46 +0200

    update categories context

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,10 +1,9 @@
 import { createContext, useState, useEffect } from "react";
-import SHOP_DATA from "../shop-data.js";
 
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
-  categoriesMap: [],
+  categoriesMap: {},
 });
 
 export const CategoriesProvider = ({ children }) => {
