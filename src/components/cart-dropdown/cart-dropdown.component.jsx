8718ba050dd1cea4883038cf87564887f4f004c4
Luciano Ramello <luchosr@gmail.com>
2022-10-06 15:53:08 +0200

    update styles

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,24 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { CartContext } from '../../context/cart.context';
+import { CartContext } from "../../context/cart.context";
 
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-import './cart-dropdown.styles.scss';
+import "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
-    navigate('/checkout');
+    navigate("/checkout");
   };
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
         {cartItems.map((item) => (
           <CartItem key={item.id} cartItem={item} />
         ))}
