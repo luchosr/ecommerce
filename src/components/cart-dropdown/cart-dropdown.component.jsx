019d98f7f540fc51963f74c75fad6cf0d38bfeaa
Luciano Ramello <luchosr@gmail.com>
2022-10-07 10:25:49 +0200

    adds reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { CartContext } from "../../context/cart.context";
+import { CategoriesContext } from "../../context/categories.context";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
