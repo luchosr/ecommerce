243e0b37c8c3ee69dff683d1e085d9fda2d8c9ac
Luciano Ramello <luchosr@gmail.com>
2022-11-15 18:53:06 +0100

    Update store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
+import { Routes, Route } from "react-router-dom";
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "./utils/firebase/firebase.utils";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 import { setCurrentUser } from "./store/user/user.action";
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from "./utils/firebase/firebase.utils";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -21,12 +22,11 @@ const App = () => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
-
       dispatch(setCurrentUser(user));
     });
 
     return unsubscribe;
-  }, [dispatch]);
+  }, []);
 
   return (
     <Routes>
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,10 @@
-import { useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
   selectCartCount,
   selectIsCartOpen,
 } from "../../store/cart/cart.selector";
-
 import { setIsCartOpen } from "../../store/cart/cart.action";
-
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import { CartIconContainer, ItemCount } from "./cart-icon.styles";
