0df8a51fbbdc7b3f8b186be8687eaa0e8e00b06d
Luciano Ramello <luchosr@gmail.com>
2022-11-30 13:46:03 +0100

    update cart-item styles

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-
-import './cart-item.styles.scss';
+import { CartItemContainer, ItemDetails } from "./cart-item.styles";
 
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <div className='cart-item-container'>
-      <img src={imageUrl} alt={name} />
-      <div className='item-details'>
-        <span className='name'>{name}</span>
-        <span className='price'>
+    <CartItemContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <span>{name}</span>
+        <span>
           {quantity} x ${price}
         </span>
-      </div>
-      <span>{quantity}</span>
-    </div>
+      </ItemDetails>
+    </CartItemContainer>
   );
 };
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from 'redux-logger';
+import { persistStore, persistReducer } from "redux-persist";
 
 import { rootReducer } from "./root-reducer";
 
