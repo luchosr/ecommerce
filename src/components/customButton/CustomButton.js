7d8b9d43a597360f53d16f076ab733982e28b7bc
Luciano Ramello <luchosr@gmail.com>
2022-12-12 10:24:27 +0100

    delete old components

--- src/components/customButton/CustomButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { CustomButtonContainer } from "./CustomButtonStyles";
-
-const CustomButton = ({ children, ...props }) => {
-  return <CustomButtonContainer {...props}>{children}</CustomButtonContainer>;
-};
-
-export default CustomButton;
