744cde9660b63455889728444a915f45dab5dd05
Luciano Ramello <luchosr@gmail.com>
2022-12-12 10:25:58 +0100

    update form input styles

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import './form-input.styles.scss';
+import "./form-input.styles.jsx";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className='group'>
-      <input className='form-input' {...otherProps} />
+    <div className="group">
+      <input className="form-input" {...otherProps} />
       {label && (
         <label
           className={`${
-            otherProps.value.length ? 'shrink' : ''
+            otherProps.value.length ? "shrink" : ""
           } form-input-label`}
         >
           {label}
