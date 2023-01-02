9df2b1ba9062ea50522cd227ee558089c910b652
Luciano Ramello <luchosr@gmail.com>
2023-01-02 11:44:31 +0100

    update index and persistor

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import logger from 'redux-logger';
 
 import { rootReducer } from "./root-reducer";
 
@@ -19,6 +21,20 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
 const middleWares = [loggerMiddleware];
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
