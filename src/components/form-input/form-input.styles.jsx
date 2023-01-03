fec0c9d057522877ab3d356eb66e173a3ee80897
Luciano Ramello <luchosr@gmail.com>
2023-01-03 14:49:03 +0100

    update form input styles and structure

--- src/components/form-input/form-input.styles.jsx
+++ src/components/form-input/form-input.styles.jsx
@@ -1,7 +1,7 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
-const subColor = 'grey';
-const mainColor = 'black';
+const subColor = "grey";
+const mainColor = "black";
 
 const shrinkLabelStyles = css`
   top: -14px;
@@ -44,7 +44,7 @@ export const Input = styled.input`
 export const Group = styled.div`
   position: relative;
   margin: 45px 0;
-  input[type='password'] {
+  input[type="password"] {
     letter-spacing: 0.3em;
   }
 `;
