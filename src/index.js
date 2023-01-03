4addc1a0708588a4d7f418df143f679ab0dfbfa4
Luciano Ramello <luchosr@gmail.com>
2023-01-03 14:26:06 +0100

    update index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import { store, persistor } from "./store/store";
+import { store } from "./store/store";
 
 import "./index.scss";
 
@@ -14,11 +13,9 @@ const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   rootElement
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from 'redux-logger';
 
 import { rootReducer } from "./root-reducer";
 
