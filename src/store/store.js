23d835d5757991c3cf9224464c38b9b39f855849
Luciano Ramello <luchosr@gmail.com>
2023-03-10 16:52:08 +0100

    Update store

--- src/store/store.js
+++ src/store/store.js
@@ -1,38 +1,29 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-import { rootReducer } from "./root-reducer";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
+import { rootReducer } from "./root-reducer";
 
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
 
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
+
 const persistConfig = {
   key: "root",
   storage,
-  blackList: ["user"],
+  blacklist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
