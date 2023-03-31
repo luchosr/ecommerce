87a7afae80d2a267fac115e11709b16dfba2ed38
Luciano Ramello <luchosr@gmail.com>
2023-03-31 18:30:09 +0200

    update categories action

--- src/store/categories/category.saga.js
+++ src/store/categories/category.saga.js
@@ -1,3 +1,8 @@
 import { takeLatest, all, call, put } from "redux-saga/effects";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+import {
+  fetchCategoriesSuccess,
+  fetchCategoriesFailure,
+} from "./category.action";
